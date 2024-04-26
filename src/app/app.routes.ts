ee11658c8468a6e9e40bab71d96cb525468c6c0f
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2024-04-26 23:06:36 +0500

    routes have been added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: "full"
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            } 
+        ]
+    }
+];
