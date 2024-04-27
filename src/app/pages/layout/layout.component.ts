1f6d207a2aaf8faf7d0826b2012cf2a45e1fc7cc
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2024-04-27 13:16:50 +0500

    layout has been added

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, 
+    RouterOutlet
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
